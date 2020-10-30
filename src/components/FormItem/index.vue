e945b288830a3343b30c343bd8a45ed0f6819f85
丁泽昭 <dingzz@jvtd.cn>
2020-10-30 18:23:07 +0800

    修改上传文件路径

--- src/components/FormItem/index.vue
+++ src/components/FormItem/index.vue
@@ -263,7 +263,7 @@ export default {
   data() {
     return {
       formData: {},
-      action: "/api/api/sysPrepaidCard/uploadFile",
+      action: "/api/sysPrepaidCard/uploadFile",
       header: {
         token: getToken(),
       },
