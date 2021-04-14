26284ceeac2d065c795b7b883c084f387e5a3680
weihaiyang <weihy@jvtd.cn>
2021-04-14 10:36:06 +0800

    bug36531 标签管理-创建后创建人取值错误问题

--- src/views/SystemManagement1/tagsManagement/index.vue
+++ src/views/SystemManagement1/tagsManagement/index.vue
@@ -14,7 +14,7 @@
     >
       <el-table-column prop="name" label="标签名称" />
       <el-table-column prop="createTime" label="创建时间" />
-      <el-table-column prop="name" label="创建人" />
+      <el-table-column prop="userName" label="创建人" />
       <el-table-column label="操作" width="200">
         <template slot-scope="scope">
           <el-button @click="_mxEdit(scope.row, 'id')" type="text" size="small"
