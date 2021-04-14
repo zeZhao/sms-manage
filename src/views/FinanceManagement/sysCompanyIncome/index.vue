a991dd30858896e05b7a4b7e8a30ad0562771e37
weihaiyang <weihy@jvtd.cn>
2021-04-14 10:27:14 +0800

    收入信息切换是否出明细应收款没有展示的问题

--- src/views/FinanceManagement/sysCompanyIncome/index.vue
+++ src/views/FinanceManagement/sysCompanyIncome/index.vue
@@ -47,9 +47,9 @@
         label="单价(分)	"
         v-if="searchParam.isDetail === 2"
       />
-      <el-table-column prop="receivableMoney" label="应收款(元)" />
-      <el-table-column prop="factMoney" label="	实收款(元)	" />
-      <el-table-column prop="poorMoney" label="欠收款(元)" />
+      <el-table-column prop="receivableMoney" label="应收款(元)" key="receivableMoney"/>
+      <el-table-column prop="factMoney" label="	实收款(元)" key="factMoney" />
+      <el-table-column prop="poorMoney" label="欠收款(元)" key="poorMoney" />
       <el-table-column
         prop="smsType"
         label="信息类型	"
