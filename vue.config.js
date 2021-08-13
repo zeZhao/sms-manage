9f21a3b06fb86cc27c65eaf046966820368d8758
weihaiyang <weihy@jvtd.cn>
2021-08-13 13:47:02 +0800

    重写代理地址

--- vue.config.js
+++ vue.config.js
@@ -50,7 +50,7 @@ module.exports = {
       //http://10.10.0.4:82 开发环境
       '/api': {
         // target: process.env.VUE_APP_HOST_URL+'/api',
-        target: 'http://smsmanage.jvtd.cn', //测试环境
+        target: 'http://manage.sms.jvtdtest.top', //测试环境
         // target: 'http://managesms.jvtd.cn', //正式环境
         // target: 'http://10.10.0.4:82', //开发环境
         // target: 'http://192.168.13.230:9091', //本地联调
@@ -58,7 +58,7 @@ module.exports = {
         pathRewrite: {
           // '^/api': 'http://192.168.13.230:9091' //重写接口访问
           // '^/api': 'http://10.10.0.4:82' //重写接口访问
-          '^/api': 'http://smsmanage.jvtd.cn' //重写接口访问
+          '^/api': 'http://manage.sms.jvtdtest.top' //重写接口访问
           // '^/api': 'http://managesms.jvtd.cn' //正式环境
         }
       }
