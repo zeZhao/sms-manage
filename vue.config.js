002c0926e5ee626467e6ebcae8ae74cd7a2a7a54
魏海洋 <weihy@jvtd.cn>
2021-08-20 18:08:32 +0800

    更新 vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -50,16 +50,16 @@ module.exports = {
       //http://10.10.0.4:82 开发环境
       '/api': {
         // target: process.env.VUE_APP_HOST_URL+'/api',
-        target: 'http://manage.sms.jvtdtest.top', //测试环境
-        // target: 'http://managesms.jvtd.cn', //正式环境
+        // target: 'http://manage.sms.jvtdtest.top', //测试环境
+        target: 'http://smsmanage.jvtd.cn', //正式环境
         // target: 'http://10.10.0.4:82', //开发环境
         // target: 'http://192.168.13.230:9091', //本地联调
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         pathRewrite: {
           // '^/api': 'http://192.168.13.230:9091' //重写接口访问
           // '^/api': 'http://10.10.0.4:82' //重写接口访问
-          '^/api': 'http://manage.sms.jvtdtest.top' //重写接口访问
-          // '^/api': 'http://managesms.jvtd.cn' //正式环境
+          // '^/api': 'http://manage.sms.jvtdtest.top' //重写接口访问
+           '^/api': 'http://smsmanage.jvtd.cn' //正式环境
         }
       }
     },
