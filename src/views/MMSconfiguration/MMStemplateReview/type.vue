87738614d9ee95894f7de1d6b40fa0742b6d5247
weihaiyang <weihy@jvtd.cn>
2021-09-27 19:05:03 +0800

    短信运营端-彩信管理-彩信模板审核预览回显图片、音频、视频去除/api（等保要求）

--- src/views/MMSconfiguration/MMStemplateReview/type.vue
+++ src/views/MMSconfiguration/MMStemplateReview/type.vue
@@ -177,11 +177,11 @@ export default {
         if (res.code === 200) {
           this.formObj = res.data;
           const data = this.typeConversion(res.data.mmsPages);
-          data.forEach((item, index) => {
-            item[index + 1].forEach(it => {
-              if (it.pageType !== 0) it.pageMedia = '/api' + it.pageMedia;
-            })
-          })
+          // data.forEach((item, index) => {
+          //   item[index + 1].forEach(it => {
+          //     if (it.pageType !== 0) it.pageMedia = '/api' + it.pageMedia;
+          //   })
+          // })
           this.formViews = data;
         } else {
           this.$message.error(res.data || res.msg);
