9dd3e760faf47f04349bbe3e1da01dd0c80d6778
weihaiyang <weihy@jvtd.cn>
2021-12-08 11:28:44 +0800

    账号管理-账户信息-信息弹窗添加账户名称

--- src/views/merchantsConfig/userManagement/index1.vue
+++ src/views/merchantsConfig/userManagement/index1.vue
@@ -457,6 +457,7 @@
         <p>产品类型: web</p>
         <p>企业名称: {{ infoData.corpName }}</p>
         <p>账户编号: {{ infoData.userId }}</p>
+        <p>账户名称: {{ infoData.userName }}</p>
         <p>web账号: {{ infoData.loginName }}</p>
         <p>
           web密码: {{ infoData.webPassword }}
@@ -473,6 +474,7 @@
         <p>企业名称: {{ infoData.corpName }}</p>
         <p>web账号: {{ infoData.loginName }}</p>
         <p>账户编号: {{ infoData.userId }}</p>
+        <p>账户名称: {{ infoData.userName }}</p>
         <p>
           密码: {{ infoData.password }}
           <i
@@ -492,6 +494,7 @@
         <p>端口: 7893</p>
         <p>web账号: {{ infoData.loginName }}</p>
         <p>账户编号: {{ infoData.userId }}</p>
+        <p>账户名称: {{ infoData.userName }}</p>
         <p>
           密码: {{ infoData.password }}
           <i
