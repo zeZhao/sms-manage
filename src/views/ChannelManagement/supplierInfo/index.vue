eee368feea32d93c1e42bf81fec98b6a6c400966
weihaiyang <weihy@jvtd.cn>
2021-12-22 18:58:29 +0800

    去除一些必填项 修改显示上条数据的bug

--- src/views/ChannelManagement/supplierInfo/index.vue
+++ src/views/ChannelManagement/supplierInfo/index.vue
@@ -269,8 +269,8 @@ export default {
       this.supplierId = row.supplierId;
       this.formConfig.forEach((item) => {
         for (let key in row) {
-          if (item.key === key && row[key] !== '-') {
-            this.$set(item, 'defaultValue', row[key]);
+          if (item.key === key) {
+            this.$set(item, 'defaultValue', row[key] !== '-' ? row[key] : '');
           }
         }
         if (!Object.keys(row).includes(item.key)) {
