e365228d94a6527b3ee0e233bfe306d0dbdff07d
zeZhao <zezhao1026@163.com>
2022-02-12 15:18:42 +0800

    修改bug

--- src/views/AuditsManagement/smsCheckWait/index.vue
+++ src/views/AuditsManagement/smsCheckWait/index.vue
@@ -61,6 +61,7 @@
           <span v-if="scope.row.source === '5'">免审数量超标</span>
           <span v-if="scope.row.source === '6'">组合redis出错</span>
           <span v-if="scope.row.source === '7'">组合超时</span>
+          <span v-if="scope.row.source === '9'">触发链接拦截</span>
         </template>
       </el-table-column>
       <!-- <el-table-column prop="cmGateway" label="移动通道">
---
+++ src/views/Csp/csp/index.vue
@@ -0,0 +1,25 @@
+!<template>
+  <div class="csp">
+    <a
+      href="https://csp.jvtd.cn/#/index"
+      target="_blank"
+      rel="noopener noreferrer"
+      >5G短信</a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {}
+};
+</script>
+<style lang="scss" scoped></style>
