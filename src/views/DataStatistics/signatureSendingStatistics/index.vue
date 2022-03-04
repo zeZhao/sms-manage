947a5ee612c67dee8cce561ee558f259a1a695e0
weihaiyang <weihy@jvtd.cn>
2022-03-04 18:31:38 +0800

    彩信发送记录/模板审核记录/报告推送记录存在查询问题;重置为null类型

--- src/views/DataStatistics/signatureSendingStatistics/index.vue
+++ src/views/DataStatistics/signatureSendingStatistics/index.vue
@@ -39,12 +39,16 @@
       </el-table-column>
     </el-table>
     <p style="color: red;font-size: 12px;">
-      提交数: {{ tabBottomData.submitCount || 0 }}&nbsp;&nbsp; 发送数:{{
+      <!-- 提交数: {{ tabBottomData.submitCount || 0 }}&nbsp;&nbsp; -->
+      发送数:{{
         tabBottomData.sendCount || 0
-      }}&nbsp;&nbsp; 成功数:{{ tabBottomData.succCount || 0 }}&nbsp;&nbsp;
-      失败数:{{ tabBottomData.failCount || 0 }}&nbsp;&nbsp; 未知数:{{
+      }}&nbsp;&nbsp;
+      成功数:{{ tabBottomData.succCount || 0 }}&nbsp;&nbsp;
+      <!-- 失败数:{{ tabBottomData.failCount || 0 }}&nbsp;&nbsp; -->
+      <!-- 未知数:{{
         tabBottomData.sendUnknownCount || 0
-      }}&nbsp;&nbsp; 成功率:
+      }}&nbsp;&nbsp; -->
+      成功率:
       {{
         tabBottomData.sendSuccPercen && tabBottomData.sendSuccPercen !== "NaN"
           ? parseFloat(tabBottomData.sendSuccPercen).toFixed(2) || 0
--- src/views/MMSconfiguration/MMStemplateReviewRecord/index.vue
+++ src/views/MMSconfiguration/MMStemplateReviewRecord/index.vue
@@ -157,6 +157,14 @@ export default {
       } else {
         return "-";
       }
+    },
+    _formatRequestData(data) {
+      for(let i in data) {
+        if (data[i] === "") {
+          data[i] = null;
+        }
+      }
+      return data;
     }
   }
 };
