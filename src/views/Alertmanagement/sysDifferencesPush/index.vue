2728426a4dcdf138b3eb07bfccad0970a7b483a7
zezhao1026@163.com <dingzz@jvtd.cn>
2023-05-09 10:03:18 +0800

    添加导出功能

--- src/views/Alertmanagement/sysDifferencesPush/index.vue
+++ src/views/Alertmanagement/sysDifferencesPush/index.vue
@@ -12,6 +12,13 @@
         <el-form-item label="账户编号">
           <el-input placeholder="请输入账户编号" v-model="searchFormPush.userId"></el-input>
         </el-form-item>
+        <el-form-item label="推送状态">
+          <!-- <el-input placeholder="请输入推送状态" v-model="searchFormPush.stateDes"></el-input> -->
+          <el-select v-model="searchFormPush.stateDes" clearable>
+            <el-option label="已推送" :value="0"></el-option>
+            <el-option label="未推送" :value="1"></el-option>
+          </el-select>
+        </el-form-item>
         <el-form-item label="是否实时统计">
           <el-select v-model="searchFormPush.realTimeStatistics">
             <el-option label="是" :value="true"></el-option>
@@ -39,6 +46,7 @@
           </div>
         </el-form-item>
         <el-button type="primary" @click="search">查询</el-button>
+        <el-button type="primary" @click="exportDiff">导出</el-button>
       </el-form>
     </div>
     <el-table
@@ -55,9 +63,9 @@
       <el-table-column prop="diff" label="差异" />
       <el-table-column prop="sendTime" label="发送时间" />
       <el-table-column prop="updateTime" label="更新时间" />
-      <el-table-column prop="stateDes" label="备注" />
+      <el-table-column prop="pushState" label="推送状态" />
       <!-- <el-table-column prop="serverNum" label="服务器编号	" /> -->
-      <!-- <el-table-column prop="count" label="	错误次数" />
+      <!-- <el-table-column prop="count" label="	错误次数" />fa
       <el-table-column
         prop="remark"
         label="备注"
@@ -151,6 +159,45 @@ export default {
           this.pageObj.total = res.data.total
         }
       })
+    },
+    //导出
+    exportDiff(){
+      const {startSendTimes} = this.searchFormPush
+      let data = {
+        startSendTime:startSendTimes? startSendTimes[0]:'',
+        endSendTime: startSendTimes?startSendTimes[1]:'',
+        ...this.searchFormPush,
+        pageNumber:this.pageObj.currentPage,
+        pageSize:this.pageObj.pageSize
+
+      }
+      this.$axios
+        .post(
+          '/retreport/push/diff/export',
+          { ...data },
+          {
+            responseType: "blob",
+            headers: { token: window.localStorage.getItem("token") }
+          }
+        )
+        .then(res => {
+          let blob = new Blob([res.data], {
+              type: "application/vnd.ms-excel;charset=utf-8"
+            });
+            let url = window.URL.createObjectURL(blob);
+            let aLink = document.createElement("a");
+            aLink.style.display = "none";
+            aLink.href = url;
+            aLink.setAttribute(
+              "download",
+              `状态差异.xlsx`
+            );
+            document.body.appendChild(aLink);
+            aLink.click();
+            document.body.removeChild(aLink);
+            window.URL.revokeObjectURL(url);
+        });
+      
     },
     /***
      * 页码改变触发
